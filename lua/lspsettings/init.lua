4d10f6b684b8480dbc5705c4fec801c9aff3fdfd
amv-dev <amv-dev@protonmail.com>
2025-06-07 00:58:13 +0500

    feat: autoupdate

--- lua/lspsettings/init.lua
+++ lua/lspsettings/init.lua
@@ -16,6 +16,24 @@ M.setup = function(opts)
         local settings = loader:load(server_name)
         config.on_init(server_name, settings)
     end
+
+    local patterns = vim.fn.map(
+        config.paths,
+        function(_, path)
+            return vim.fs.joinpath(path, "*.json")
+        end
+    )
+
+    vim.api.nvim_create_autocmd("BufWrite", {
+        pattern = patterns,
+        callback = function(event_data)
+            local file = event_data.file
+            local fname = vim.fs.basename(file)
+            local server_name = fname:sub(1, -6)
+            local settings = loader:load(server_name)
+            config.on_update(server_name, settings)
+        end
+    })
 end
 
 return M
